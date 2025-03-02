6806d413737bef1ba5f0101902dc7c068008c0c1
Haresh T Suppiah <haresh@humanperformance.sg>
2025-03-02 17:28:52 +1100

    Fix Vite configuration and file structure

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 export default {
-  base: '/',
+  base: './',
+  publicDir: 'public',
   server: {
     port: 3000
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: '/index.html'
+      }
+    }
   }
 } 
